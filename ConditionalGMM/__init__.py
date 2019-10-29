3e63174331a817dfd11a6ba968b6470ee07f8759
tmcclintock <tmcclintock89@gmail.com>
2019-10-29 14:30:54 -0400

    Implemented and tested the univariate GMM, at least with basic properties.

--- ConditionalGMM/__init__.py
+++ ConditionalGMM/__init__.py
@@ -1,5 +1,6 @@
 from .MNorm import *
 from .condGMM import *
+from .UnivariateGMM import *
 
 __author__ = "Tom McClintock <thmsmcclintock@gmail.com>"
 __version__ = 0.1
---
+++ tests/test_UGMM.py
@@ -0,0 +1,22 @@
+"""Tests of the univariate GMM
+"""
+from ConditionalGMM import UnivariateGMM
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+def test_ugmm_basic():
+    #Smoke tests
+    weights = [0.5, 0.5]
+    means = [0., 1.]
+    vars = [1., 2.]
+    ugmm = UnivariateGMM.UniGMM(weights, means, vars)
+
+def test_statistical_moments():
+    weights = [0.5, 0.5]
+    means = [0., 0.]
+    vars = [1., 2.]
+    ugmm = UnivariateGMM.UniGMM(weights, means, vars)
+
+    npt.assert_equal(ugmm.mean(), 0)
+    npt.assert_equal(ugmm.median(), 0)
